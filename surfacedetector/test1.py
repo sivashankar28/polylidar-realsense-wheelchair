56fccaf6a5179a6aec556f0720a171c9baf8fb07
saran230420 <sivashankar@me.com>
2020-08-09 20:31:47 -0400

    arduino

--- surfacedetector/test1.py
+++ surfacedetector/test1.py
@@ -1,17 +1,11 @@
+#!/usr/bin/env python3
 import serial
 import time
-
-ser= serial.Serial('/dev/ttyACM0', baudrate= 9600)
-# time.sleep(3)
-
-# def getValues() :
-#      ser.write(b'g')
-#      arduinoData= ser.readline().decode('ascii')
-#      return arduinoData
-
-
-while (1):
-     # userInput = input ('Get Data Point?')
-     # if userInput == 'y':
-     ser.write(b'Hello')
-     #    print(getValues())
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.flush()
+    while True:
+        ser.write(b"Hello from Raspberry Pi!\n")
+        # line = ser.readline().decode('utf-8').rstrip()
+        # print(line)
+        time.sleep(1)
